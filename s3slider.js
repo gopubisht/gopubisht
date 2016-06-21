6de35b2fe0fb1d0e4d8c45e87814b6e07cefbd12
gopubisht <gopubisht@users.noreply.github.com>
2016-06-21 15:41:13 +0530

    Update s3slider.js
    this is update file

--- s3slider.js
+++ s3slider.js
@@ -1,10 +1,10 @@
 /*-----------------------------------------------
 Theme Name: Daily Times
-Designer : Hardeep Singh
-Created: Feb, 2014
-Theme URI: http://hackerschronicle.com
-Author: Hardeep Singh
-Author URI: http://hackerschronicle.com
+Designer : Gopu Bisht
+Created: Mar, 2014
+Theme URI: http://devbhoomiuttarakhand.com
+Author: Gopu Bisht
+Author URI: http://gopubisht.blogspot.com
 ----------------------------------------------- */
 (function($){$.fn.s3Slider=function(vars){var element=this;var timeOut=(vars.timeOut!=undefined)?vars.timeOut:4000;var current=null;var timeOutFn=null;var faderStat=true;var mOver=false;var items=$("#"+ element[0].id+"Content ."+ element[0].id+"Image");var itemsSpan=$("#"+ element[0].id+"Content ."+ element[0].id+"Image span");items.each(function(i){$(items[i]).mouseover(function(){mOver=true;});$(items[i]).mouseout(function(){mOver=false;fadeElement(true);});});var fadeElement=function(isMouseOut){var thisTimeOut=(isMouseOut)?(timeOut/2):timeOut;thisTimeOut=(faderStat)?10:thisTimeOut;if(items.length>0){timeOutFn=setTimeout(makeSlider,thisTimeOut);}else{console.log("Poof..");}}
 var makeSlider=function(){current=(current!=null)?current:items[(items.length-1)];var currNo=jQuery.inArray(current,items)+ 1
