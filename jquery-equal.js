8c6ac5ae40ae1e7da98382fb2effdf1b60809cf9
gopubisht <gopubisht@users.noreply.github.com>
2016-06-21 15:34:41 +0530

    Update jquery-equal.js
    This is updated

--- jquery-equal.js
+++ jquery-equal.js
@@ -1,9 +1,9 @@
 /*-----------------------------------------------
 Theme Name: Daily Times
-Designer : Hardeep Singh
-Created: Feb, 2014
-Theme URI: http://hackerschronicle.com
-Author: Hardeep Singh
-Author URI: http://hackerschronicle.com
+Designer : Gopu Bisht
+Created: Mar, 2014
+Theme URI: http://devbhoomiuttarakhand.com
+Author: Gopu Bisht
+Author URI: http://gopubisht.blogspot.com
 ----------------------------------------------- */
 (function($){$.fn.equalHeights=function(minHeight,maxHeight){tallest=(minHeight)?minHeight:0;this.each(function(){if($(this).height()>tallest){tallest=$(this).height();}});if((maxHeight)&&tallest>maxHeight)tallest=maxHeight;return this.each(function(){$(this).height(tallest).css("overflow","auto");});}})(jQuery);
